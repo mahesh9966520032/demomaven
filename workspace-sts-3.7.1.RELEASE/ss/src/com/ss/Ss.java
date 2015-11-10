f5425e082816bc6e8eeaab69b8b1f02bc8a3b0bf
mahesh <mahesh@mahesh-Vostro-3558>
2015-11-10 07:44:01 +0530

    this is first commit

---
+++ workspace-sts-3.7.1.RELEASE/ss/src/com/ss/Ss.java
@@ -0,0 +1,37 @@
+package com.ss;
+
+
+
+
+
+  public class Ss {
+
+public static boolean login()
+{
+        return true;	
+
+
+}
+
+public static boolean logonTo()
+{
+	
+	if(!login())
+		
+	return false;
+	
+	System.out.println("test");
+	
+	return true;
+	
+}
+	
+	public static void main(String[] args) 
+	
+	{
+
+		System.out.println(logonTo());;
+
+	}
+
+}
